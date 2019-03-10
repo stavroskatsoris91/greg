9e0371f57e939d08302e2c1a908a3b89f394b2cf
Stavros Katsoris <stavros@stavros-mbp.home>
2019-03-10 11:38:22 +0000

    update

---
+++ flapper-news/app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ActionController::Base
+    def angular
+        render 'layouts/application'
+    end
+end
---
+++ flapper-news/db/migrate/20190310105607_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :link
+      t.integer :upvotes
+
+      t.timestamps
+    end
+  end
+end
