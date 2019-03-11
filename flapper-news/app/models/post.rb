89c2a6b09cb698fe1a52ac233edecbc7764652a5
Stavros Katsoris <stavros@Stavros-MacBook-Pro.local>
2019-03-11 09:14:31 +0000

    finish tutorial

--- flapper-news/app/models/post.rb
+++ flapper-news/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
+    belongs_to :user
     has_many :comments
     def as_json(options = {})
-        super(options.merge(include: :comments))
-    end
+    super(options.merge(include: [:user, comments: {include: :user}]))
+  end
 end
--- flapper-news/db/schema.rb
+++ flapper-news/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_10_105710) do
+ActiveRecord::Schema.define(version: 2019_03_10_215950) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 2019_03_10_105710) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -27,6 +29,22 @@ ActiveRecord::Schema.define(version: 2019_03_10_105710) do
     t.integer "upvotes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
