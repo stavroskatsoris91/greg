04defee9548bc42bd6b53ce422be4c51a07309fa
Stavros Katsoris <stavroskatsoris@thortfuls-mbp.home>
2020-06-21 18:37:01 +0100

    update check list

--- gregs-app/src/app/components/instructors/instructors.component.ts
+++ gregs-app/src/app/components/instructors/instructors.component.ts
@@ -9,19 +9,19 @@ export class InstructorsComponent implements OnInit {
 
   instructors=[
     {
-      name: 'GREG',
+      name: 'Greg',
       color: 'red-1',
-      description:'Greg is the owner of Greg’s Spetses Horses. He is half English and half Greek and he speaks both languages fluently. He has trained all of our horses. He likes to teach his students the importance of building a compassionate relationship between the rider and the horse. He’s the main trek guide and enjoys showing visitors the beautiful island he grew up in.'
+      description:'Greg is the owner of Greg’s Spetses Horses. He was raised in Spetses and he is half English, half Greek and speaks both languages fluently. Greg has trained all our horses and he is also the main performer in our new “Greg’s Spetses Horses” Show with his horse, Olivia. As an instructor, He likes to teach his students the importance of building a compassionate relationship with the horses they ride. He’s the trek guide and enjoys showing visitors the beautiful island he grew up in. Apart from our equestrian club, Greg also drives a beautiful horse drawn Victoria carriage which are traditional to the island.'
     },
     {
-      name: 'TRACY',
+      name: 'Tracy',
       color: 'yellow-3',
-      description:'Tracy was born and raised in England and she has been living in Spetses for nearly 30 years. She’s fluent in English and Greek. She trained with the BHS in horsemanship and teaching. She loves to teach and see the progress of her students.'
+      description:'Tracy was born and raised in England and she has been living in Spetses for nearly 30 years and speaks fluent both English and Greek. She trained with the British Horse Society in horsemanship and teaching. Tracy also enjoyed show jumping and cross country back in England. Being an instructor, she loves to teach and see the progress of her students.'
     },
     {
-      name: 'KATERINA',
+      name: 'Katerina',
       color: 'purple-3',
-      description:'Katerina is Greek and was raised in Spetses. She’s fluent in English and Greek. She’s trained with Greg for the last four years. She is a riding instructress and a trek guide. She’s in the third year of studying for a degree in nursing.'
+      description:'Katerina is Greek and was raised in Spetses. Growing up on a Greek island she started learning English from a young age and she is now fluent in both languages. She\'s been part of the Greg\'s Spetses Horses team from the beginning. She is a riding instructress, assists on treks and is administrator, taking care of the website, advertising and social media. Katerina performs in the “Greg’s Spetses Horses” show and she loves liberty training with her horse, Robbie. Apart from her love for horses, she’s in her third year of studying for a degree in nursing.'
     }
   ];
   constructor() { }
